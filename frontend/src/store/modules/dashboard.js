b16286ffa8fe65f20759278b18af39be637d235b
Yanick Assignon <assignon>
2021-03-16 06:32:41 +0100

    flask api settings done

---
+++ frontend/src/store/modules/dashboard.js
@@ -0,0 +1,22 @@
+export default {
+    namespaced: true,
+
+    state: {
+        
+    },
+
+    getters: {
+        // getUserData(state) {
+        //     return state.userDataArr
+        // },
+    },
+
+    mutations: {
+    
+    },
+
+    actions: {
+        
+    },
+  
+}
