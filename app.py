7b7b0154d2aaaf3c976364ad112f04562d66c4dd
Yanick Assignon <assignon>
2021-03-16 06:36:05 +0100

    flask api settings done

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-# from flask_restful import Api
+# from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from settings import app
+from routes import api_routes
 
-app = Flask(__name__, static_url_path='/static', template_folder="templates")
 app.secret_key = "sunnySideUp-smartMirror"
+# bcrypt = Bcrypt(app)
+rest_api = Api(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
-def run_app():
-    app.run()
+api_routes(rest_api)
 
 
 if __name__ == 'main':
-    run_app()
+    app.run()
 
 
 
---
+++ resources/user_api.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask import request
+from flask_jwt_extended import jwt_required
+# models imports
+from models.user_model import *
+
+model = UserModel()
+
+class CreateUser(Resource):
+    def post(self):
+        pass
+
+class GetUser(Resource):
+    @jwt_required
+    def get(self):
+       return model.get_user()
