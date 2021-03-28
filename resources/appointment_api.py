8fa7be3c89e1b48dbb6bad1f8c2b62259e8a6172
TientjieIT <tientjieit@gmail.com>
2021-03-28 22:40:46 +0200

    appointments post en put done

--- resources/appointment_api.py
+++ resources/appointment_api.py
@@ -1,11 +1,100 @@
 from flask_restful import Resource, reqparse, abort
 from flask import request, jsonify, make_response
-from settings import ma
+from settings import ma, db
 from marshmallow import ValidationError
+from sqlalchemy import exc
+from sqlalchemy.orm.exc import NoResultFound
 from .authentication_api import login_required
+from schemas.appointment_schema import AppointmentSchema, EditAppointmentSchema
+from models.appointment_model import Appointment
+
+
+appointment_schema = AppointmentSchema()
+appointments_schema = AppointmentSchema(many=True)
+edit_appointment_schema = EditAppointmentSchema()
+
 
 class AppointmentCollection(Resource):
-    pass
+    
+    @staticmethod
+    @login_required
+    def get():
+        """
+        nog niet zeker wat dit moet gaan doen
+        """
+        pass
+
+    @staticmethod
+    @login_required
+    def post(current_user):
+        """
+        Creates a new appointment in the appointment table
+        """
+        json_data = request.get_json()
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+        
+        # remove whitespaces from input
+        
+        remove_whitespace(json_data)
+
+        # Validate and deserialize input
+        
+        try:
+            data = appointment_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 422
+        
+        try: 
+            appointment = Appointment.create(**data)
+        except exc.IntegrityError as e:
+            db.session.rollback()
+            return{'error': e.orig.args}
+        
+        return appointment_schema.dump(appointment), 201
+    
+    
+    @staticmethod
+    @login_required
+    def delete(current_user, appoinment_id):
+        Appointment.delete_appointment(appoinment_id)
+        return 200
+
 
 class AppointmentApi(Resource):
-    pass
+    
+
+    @staticmethod
+    @login_required
+    def put(current_user, appoinment_id):
+
+        json_data = request.get_json()
+
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+        
+        # remove whitespaces from input
+        
+        remove_whitespace(json_data)
+
+        # Validate and deserialize input
+        
+        try:
+            data = edit_appointment_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 422
+
+        try:        
+            edited_appointment = Appointment.update_guest(appoinment_id, **data)
+        except exc.IntegrityError as e:
+            db.session.rollback()
+            return{'error': e.orig.args}
+        except NoResultFound:
+            return{'error': 'Guest does not exist'}
+
+        return appointment_schema.dump(edited_appointment), 200
+
+def remove_whitespace(json_data):
+    for key, value in json_data.items():
+        if type(value) == str:
+            json_data[key] = value.strip()
--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 from settings import rest_api
 # models imports
-from resources.user_api import UserApi, UserCollection
+from resources.user_api import UserCollection, UserApi
 from resources.authentication_api import Login, Logout
 from resources.guest_api import GuestCollection, GuestApi
+from resources.appointment_api import AppointmentCollection, AppointmentApi
 
 
 def api_routes(rest_api):
@@ -12,7 +13,9 @@ def api_routes(rest_api):
         {'name': UserApi, 'path': '/user/<int:user_id>'},
         {'name': Login, 'path': '/login'},
         {'name': GuestCollection, 'path': ('/guests', '/guests/<int:guest_id>')},
-        {'name': GuestApi, 'path': '/guest/<int:guest_id>' }
+        {'name': GuestApi, 'path': '/guest/<int:guest_id>'},
+        {'name': AppointmentCollection, 'path': ('/appointments', '/appointments/<int:appointment_id>')},
+        {'name': AppointmentApi, 'path': ('/appointment', '/appointment/<int:appointment_id>')}
     ]
     # add api routes and make it accessible
     for route in routes:
