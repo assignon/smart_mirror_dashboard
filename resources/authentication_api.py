975e924f9410a28d7ed55d849acaa7a88379dd2d
TientjieIT <tientjieit@gmail.com>
2021-03-30 17:39:36 +0200

    try decode encode

--- resources/authentication_api.py
+++ resources/authentication_api.py
@@ -43,7 +43,10 @@ class Login(Resource):
             return {'message': 'User does not exist'}
 
         if user.check_password(auth.password):
-            token = jwt.encode({'user_id': user.user_id, 'exp': datetime.datetime.utcnow()+ datetime.timedelta(days=1)}, app.config['JWT_SECRET_KEY']).decode('utf-8')
+            try:
+                token = jwt.encode({'user_id': user.user_id, 'exp': datetime.datetime.utcnow()+ datetime.timedelta(days=1)}, app.config['JWT_SECRET_KEY']).decode('utf-8')
+            except:
+                token = jwt.encode({'user_id': user.user_id, 'exp': datetime.datetime.utcnow()+ datetime.timedelta(days=1)}, app.config['JWT_SECRET_KEY'])
             return jsonify({'x-access-token': token})
         return make_response("Could not verify", 401, {'WWW-Authenticate': ' Basic realm="Login required!"'})
 
