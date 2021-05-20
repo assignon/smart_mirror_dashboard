4d2d3c6a86a6900340ee5d535218529d3e485939
Wesley <wesley.vuong@student.hu.nl>
2021-05-20 11:44:22 +0200

    message

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit, send
-# from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 from flask_restful import Api
 from settings import app, rest_api
 from routes import api_routes
 
 app.secret_key = "sunnySideUp-smartMirror"
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 rest_api = Api(app)
-socketio = SocketIO(app, cors_allowed_origins='http://localhost:8080')
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 CORS(app)
 cors = CORS(app, resources={
     r"/*": {
-        "origins": "*"
+        "Access-Control-Allow-Origin": "*"
     }
 })
 
@@ -23,40 +23,40 @@ cors = CORS(app, resources={
 def hello_world():
     return 'Hello, World!'
 
-@socketio.on('connect')
-def user_connect():
-    print('user connected')
-    send({'msg': 'user connected'}, json=True)
-    
-@socketio.on('new_user')
-def subscribe_user(data):
-    """
-    add user in SocketUserManager DB and socket
-
-    Args:
-        data (obj): [login user id]
-    """
-    # handle connected user to the socket
-    print('new user', data)
-    
-@socketio.on('disconnect')
-def user_disconnect():
-    print('Client disconnected')
-    send({'msg': 'user disconnected'})
-    
-@socketio.on('user_disconnected')
-def unsubscribe_user(data):
-    """
-    remove user fron SocketUserManager DB and socket
-
-    Args:
-        data (obj): [logout user id]
-    """
-    print('diconnected use', data)
+# @socketio.on('connect')
+# def user_connect():
+#     print('user connected')
+#     send({'msg': 'user connected'}, json=True)
+#
+# @socketio.on('new_user')
+# def subscribe_user(data):
+#     """
+#     add user in SocketUserManager DB and socket
+#
+#     Args:
+#         data (obj): [login user id]
+#     """
+#     # handle connected user to the socket
+#     print('new user', data)
+#
+# @socketio.on('disconnect')
+# def user_disconnect():
+#     print('Client disconnected')
+#     send({'msg': 'user disconnected'})
+#
+# @socketio.on('user_disconnected')
+# def unsubscribe_user(data):
+#     """
+#     remove user fron SocketUserManager DB and socket
+#
+#     Args:
+#         data (obj): [logout user id]
+#     """
+#     print('diconnected use', data)
 
 api_routes(rest_api)
 
 
 if __name__ == 'main':
-    socketio.run(app)
-    # app.run()
+    # socketio.run(app)
+    app.run()
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -28,7 +28,7 @@ Vue.use(VueSession, { persist: true });
 
 Axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 Axios.defaults.xsrfCookieName = "XCSRF-TOKEN";
-Axios.defaults.baseURL = "http://127.0.0.1:5000";
+Axios.defaults.baseURL = "http://127.0.0.1:5000/";
 Axios.defaults.withCredentials = false;
 
 Vue.prototype.$axios = Axios;
