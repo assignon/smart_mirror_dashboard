5aa7cd32cd3173fb5cb064edf650f571927cc5d2
Sebastiaan <tientjieit@gmail.com>
2021-05-21 13:24:12 +0200

    logica fout gefixt in user_api, admin kon data van anderen niet veranderen

--- resources/user_api.py
+++ resources/user_api.py
@@ -127,7 +127,7 @@ class UserApi(Resource):
         """
         Edit user. Users can only edit their own user settings, exception is made for admins
         """
-        if current_user.user_id != user_id and current_user.is_admin is not False:
+        if current_user.user_id != user_id and current_user.is_admin is False:
             return {"message": "Not authorized to edit this user!"}
 
         json_data: dict = request.get_json()
@@ -138,13 +138,19 @@ class UserApi(Resource):
 
         remove_whitespace(json_data)
 
-        # Validate and deserialize input
+        if current_user.user_id == user_id:
+            user_to_be_updated = current_user
+        else:
+            try: 
+                user_to_be_updated = User.get_user(user_id)
+            except NoResultFound:
+                return {'message': 'User does not exist!'}
 
         try:
-            if json_data['new_password'] != '' and current_user.check_password(json_data['password']):
+            if json_data['new_password'] != '' and user_to_be_updated.check_password(json_data['password']):
                 data = edit_user_password_schema.load(json_data)
                 data['password'] = data.pop('new_password')
-            elif json_data['new_password'] != '' and not current_user.check_password((json_data['password'])):
+            elif json_data['new_password'] != '' and not user_to_be_updated.check_password((json_data['password'])):
                 return jsonify({'error': 'incorrect password'})
             else:
                 json_data.pop('new_password', None)
