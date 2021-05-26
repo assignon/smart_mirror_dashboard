d6ac28c614d3d68f86eb4ec3083ad2168fd61d7a
Sebastiaan <tientjieit@gmail.com>
2021-05-26 17:46:17 +0200

    cache api gemaakt

---
+++ resources/cache_api.py
@@ -0,0 +1,62 @@
+from flask_restful import Resource, reqparse, abort
+from flask import request, jsonify, make_response
+from settings import ma, db, redis_db
+from marshmallow import ValidationError
+from sqlalchemy import exc
+from sqlalchemy.orm.exc import NoResultFound
+from .authentication_api import login_required
+from .helper import remove_whitespace
+# models imports
+
+from models.guest_model import Guest
+
+
+class RedisCache(Resource):
+
+    @staticmethod
+    @login_required
+    def get(current_user):
+        """
+        get all cache_ids with unix time
+        """
+        cache_ids_with_unixtimes = redis_db.get_cache_ids_with_unixtime()
+        return {"cache_ids_with_unixtime": cache_ids_with_unixtimes}
+
+    @staticmethod
+    @login_required
+    def put(current_user, cache_id=None):
+        """
+        label the cached embeddings of specified cache
+        """
+        if not cache_id:
+            return {"error": "cache_id is missing in path"}
+
+        if cache_id not in redis_db.get_cache_ids():
+            return {"error": "invalid cache_id!"}
+        print(Guest.get_guest(10000))
+        json_data: dict = request.get_json()
+        if not json_data:
+            return {" error": "JSON missing :("}
+        if 'body' not in json_data.keys():
+            return {"error": "body key missing!"}
+        json_data = json_data['body']
+
+        if 'guest_id' not in json_data.keys():
+            return {"error": "guest_id is missing!"}
+
+        try:
+            guest = Guest.get_guest(json_data['guest_id'])
+        except NoResultFound:
+            return {"error": "Specified guest_id does not exist in the database"}
+
+        redis_db.label_unknown_embeddings(cache_id, guest.guest_id)
+        return 200
+
+    @staticmethod
+    @login_required
+    def delete(current_user, cache_id=None):
+        """Delete specified cache"""
+        if not cache_id:
+            return {"error": "cache_id is missing in path"}
+        redis_db.delete_unknown_cache(cache_id)
+        return 200
--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_marshmallow import Marshmallow
 from flask_swagger_ui import get_swaggerui_blueprint
-import redis
+from RedisDB.redisdb import RedisDatabase
 
 
 
@@ -60,8 +60,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Redis connection
-redis_db = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'),
-                      password=os.getenv('REDIS_PASSWORD'))
+redis_db = RedisDatabase(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'),
+                         password=os.getenv('REDIS_PASSWORD'))
 
 # api settings
 rest_api = Api(app)
