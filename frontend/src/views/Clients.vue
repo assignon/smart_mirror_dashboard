d50f0a163f2bd87e017d27ffc126fab0a35451e7
Wes <wes>
2021-06-14 18:19:00 +0200

    Empty if error fix

--- frontend/src/views/Clients.vue
+++ frontend/src/views/Clients.vue
@@ -428,7 +428,7 @@ export default {
         xaccesstoken: self.$session.get("token"),
         callback: function (res) {
           if (res.error) {
-
+            console.log(res)
           } else {
             self.$router.push("/ingecheckt");
           }
